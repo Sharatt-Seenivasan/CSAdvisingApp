907a53147b4777633221b95b0bdb6452b7753f46
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-03-12 22:52:06 -0500

    - added blank degree requirements activity - linked degree requirements button on main activity to degree requirements activity

--- app/src/main/java/net/cmsc_434/cs_advising_app/MainActivity.java
+++ app/src/main/java/net/cmsc_434/cs_advising_app/MainActivity.java
@@ -3,12 +3,26 @@ package net.cmsc_434.cs_advising_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public Button degreeRequirementButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        degreeRequirementButton = (Button)findViewById(R.id.button10);
+
+        degreeRequirementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,DegreeRequirementsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
