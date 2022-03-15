190c1393d9f4d6774bceea0301f17af96cefa7c5
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-03-14 23:13:48 -0400

    - added wireframe for appointment activity

---
+++ app/src/main/java/net/cmsc_434/cs_advising_app/AppointmentActivity.java
@@ -0,0 +1,18 @@
+package net.cmsc_434.cs_advising_app;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class AppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
