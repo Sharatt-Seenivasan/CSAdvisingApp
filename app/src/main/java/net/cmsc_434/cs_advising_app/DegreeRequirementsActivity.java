242c015fbca23733a1e1035dae0c1edc54088f5d
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-03-18 02:23:04 -0400

    - redid layout for degree requirement page - added umd_red color

--- app/src/main/java/net/cmsc_434/cs_advising_app/DegreeRequirementsActivity.java
+++ app/src/main/java/net/cmsc_434/cs_advising_app/DegreeRequirementsActivity.java
@@ -1,14 +1,20 @@
 package net.cmsc_434.cs_advising_app;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DegreeRequirementsActivity extends AppCompatActivity {
+
+    public Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_degree_requirements);
+
+        nextButton = (Button)findViewById(R.id.nextButton);
+
     }
 }
