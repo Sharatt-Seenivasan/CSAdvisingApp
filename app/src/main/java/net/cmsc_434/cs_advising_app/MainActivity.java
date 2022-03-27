7cc450b4a142fcbabbb704a054e9b78de581cb94
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-03-27 00:49:19 -0400

    set action bar to red and centered text

--- app/src/main/java/net/cmsc_434/cs_advising_app/MainActivity.java
+++ app/src/main/java/net/cmsc_434/cs_advising_app/MainActivity.java
@@ -1,11 +1,17 @@
 package net.cmsc_434.cs_advising_app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public Button degreeRequirementButton;
@@ -14,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        setTitle("University of Maryland");
+        centerTitle();
+
+
 
         degreeRequirementButton = (Button)findViewById(R.id.button10);
 
@@ -24,5 +36,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+    }
+
+    private void centerTitle() {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0) {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1) {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            } else {
+                for(View v : textViews) {
+                    if(v.getParent() instanceof Toolbar) {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null) {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
     }
 }
---
+++ app/src/main/java/net/cmsc_434/cs_advising_app/UpperLevelConcentrationRequirements.java
@@ -0,0 +1,14 @@
+package net.cmsc_434.cs_advising_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpperLevelConcentrationRequirements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upper_level_concentration_requirements);
+    }
+}
