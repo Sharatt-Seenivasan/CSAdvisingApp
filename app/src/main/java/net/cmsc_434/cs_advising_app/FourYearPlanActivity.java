3cd7780aae26a32131d971d2779875fdc3cda2f8
Pranav Pappala <pranavpappala@gmail.com>
2022-04-12 20:51:03 -0400

    done w/ spinners

--- app/src/main/java/net/cmsc_434/cs_advising_app/FourYearPlanActivity.java
+++ app/src/main/java/net/cmsc_434/cs_advising_app/FourYearPlanActivity.java
@@ -2,104 +2,174 @@ package net.cmsc_434.cs_advising_app;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.PopupMenu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+
 public class FourYearPlanActivity extends AppCompatActivity {
-    public Button class1_1 = (Button)findViewById(R.id.addClass1_1);
-    public Button class1_2 = (Button)findViewById(R.id.addClass1_2);
-    public Button class1_3 = (Button)findViewById(R.id.addClass1_3);
-    public Button class1_4 = (Button)findViewById(R.id.addClass1_4);
-    public Button class1_5 = (Button)findViewById(R.id.addClass1_5);
-    public Button class2_1 = (Button)findViewById(R.id.addClass2_1);
-    public Button class2_2 = (Button)findViewById(R.id.addClass2_2);
-    public Button class2_3 = (Button)findViewById(R.id.addClass2_3);
-    public Button class2_4 = (Button)findViewById(R.id.addClass2_4);
-    public Button class2_5 = (Button)findViewById(R.id.addClass2_5);
-    public Button class3_1 = (Button)findViewById(R.id.addClass3_1);
-    public Button class3_2 = (Button)findViewById(R.id.addClass3_2);
-    public Button class3_3 = (Button)findViewById(R.id.addClass3_3);
-    public Button class3_4 = (Button)findViewById(R.id.addClass3_4);
-    public Button class3_5 = (Button)findViewById(R.id.addClass3_5);
-    public Button class4_1 = (Button)findViewById(R.id.addClass4_1);
-    public Button class4_2 = (Button)findViewById(R.id.addClass4_2);
-    public Button class4_3 = (Button)findViewById(R.id.addClass4_3);
-    public Button class4_4 = (Button)findViewById(R.id.addClass4_4);
-    public Button class4_5 = (Button)findViewById(R.id.addClass4_5);
-    public Button class5_1 = (Button)findViewById(R.id.addClass5_1);
-    public Button class5_2 = (Button)findViewById(R.id.addClass5_2);
-    public Button class5_3 = (Button)findViewById(R.id.addClass5_3);
-    public Button class5_4 = (Button)findViewById(R.id.addClass5_4);
-    public Button class5_5 = (Button)findViewById(R.id.addClass5_5);
-    public Button class6_1 = (Button)findViewById(R.id.addClass6_1);
-    public Button class6_2 = (Button)findViewById(R.id.addClass6_2);
-    public Button class6_3 = (Button)findViewById(R.id.addClass6_3);
-    public Button class6_4 = (Button)findViewById(R.id.addClass6_4);
-    public Button class6_5 = (Button)findViewById(R.id.addClass6_5);
-    public Button class7_1 = (Button)findViewById(R.id.addClass7_1);
-    public Button class7_2 = (Button)findViewById(R.id.addClass7_2);
-    public Button class7_3 = (Button)findViewById(R.id.addClass7_3);
-    public Button class7_4 = (Button)findViewById(R.id.addClass7_4);
-    public Button class7_5 = (Button)findViewById(R.id.addClass7_5);
-    public Button class8_1 = (Button)findViewById(R.id.addClass8_1);
-    public Button class8_2 = (Button)findViewById(R.id.addClass8_2);
-    public Button class8_3 = (Button)findViewById(R.id.addClass8_3);
-    public Button class8_4 = (Button)findViewById(R.id.addClass8_4);
-    public Button class8_5 = (Button)findViewById(R.id.addClass8_5);
+    public Spinner class1_1;
+    public Spinner class1_2;
+    public Spinner class1_3;
+    public Spinner class1_4;
+    public Spinner class1_5;
+    public Spinner class2_1;
+    public Spinner class2_2;
+    public Spinner class2_3;
+    public Spinner class2_4;
+    public Spinner class2_5;
+    public Spinner class3_1;
+    public Spinner class3_2;
+    public Spinner class3_3;
+    public Spinner class3_4;
+    public Spinner class3_5;
+    public Spinner class4_1;
+    public Spinner class4_2;
+    public Spinner class4_3;
+    public Spinner class4_4;
+    public Spinner class4_5;
+    public Spinner class5_1;
+    public Spinner class5_2;
+    public Spinner class5_3;
+    public Spinner class5_4;
+    public Spinner class5_5;
+    public Spinner class6_1;
+    public Spinner class6_2;
+    public Spinner class6_3;
+    public Spinner class6_4;
+    public Spinner class6_5;
+    public Spinner class7_1;
+    public Spinner class7_2;
+    public Spinner class7_3;
+    public Spinner class7_4;
+    public Spinner class7_5;
+    public Spinner class8_1;
+    public Spinner class8_2;
+    public Spinner class8_3;
+    public Spinner class8_4;
+    public Spinner class8_5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayList<Spinner> spinners = new ArrayList<Spinner>();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four_year_plan);
         setTitle("Four Year Plan Builder");
-        registerAllButtonsForMenu();
+        populateSpinners(spinners);
+        ArrayAdapter a = ArrayAdapter.createFromResource(this, R.array.course_selection, android.R.layout.simple_spinner_item);
+        setAdapters(spinners, a);
+
+        for (Spinner s : spinners) {
+            s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+        }
     }
 
-    private void registerAllButtonsForMenu() {
-        registerForContextMenu(class1_1);
-        registerForContextMenu(class1_2);
-        registerForContextMenu(class1_3);
-        registerForContextMenu(class1_4);
-        registerForContextMenu(class1_5);
-        registerForContextMenu(class2_1);
-        registerForContextMenu(class2_2);
-        registerForContextMenu(class2_3);
-        registerForContextMenu(class2_4);
-        registerForContextMenu(class2_5);
-        registerForContextMenu(class3_1);
-        registerForContextMenu(class3_2);
-        registerForContextMenu(class3_3);
-        registerForContextMenu(class3_4);
-        registerForContextMenu(class3_5);
-        registerForContextMenu(class4_1);
-        registerForContextMenu(class4_2);
-        registerForContextMenu(class4_3);
-        registerForContextMenu(class4_4);
-        registerForContextMenu(class4_5);
-        registerForContextMenu(class5_1);
-        registerForContextMenu(class5_2);
-        registerForContextMenu(class5_3);
-        registerForContextMenu(class5_4);
-        registerForContextMenu(class5_5);
-        registerForContextMenu(class6_1);
-        registerForContextMenu(class6_2);
-        registerForContextMenu(class6_3);
-        registerForContextMenu(class6_4);
-        registerForContextMenu(class6_5);
-        registerForContextMenu(class7_1);
-        registerForContextMenu(class7_2);
-        registerForContextMenu(class7_3);
-        registerForContextMenu(class7_4);
-        registerForContextMenu(class7_5);
-        registerForContextMenu(class8_1);
-        registerForContextMenu(class8_2);
-        registerForContextMenu(class8_3);
-        registerForContextMenu(class8_4);
-        registerForContextMenu(class8_5);
+    private void populateSpinners(ArrayList<Spinner> spinnerList) {
+        class1_1 = findViewById(R.id.addClass1_1);
+        spinnerList.add(class1_1);
+        class1_2 = findViewById(R.id.addClass1_2);
+        spinnerList.add(class1_2);
+        class1_3 = findViewById(R.id.addClass1_3);
+        spinnerList.add(class1_3);
+        class1_4 = findViewById(R.id.addClass1_4);
+        spinnerList.add(class1_4);
+        class1_5 = findViewById(R.id.addClass1_5);
+        spinnerList.add(class1_5);
+        class2_1 = findViewById(R.id.addClass2_1);
+        spinnerList.add(class2_1);
+        class2_2 = findViewById(R.id.addClass2_2);
+        spinnerList.add(class2_2);
+        class2_3 = findViewById(R.id.addClass2_3);
+        spinnerList.add(class2_3);
+        class2_4 = findViewById(R.id.addClass2_4);
+        spinnerList.add(class2_4);
+        class2_5 = findViewById(R.id.addClass2_5);
+        spinnerList.add(class2_5);
+        class3_1 = findViewById(R.id.addClass3_1);
+        spinnerList.add(class3_1);
+        class3_2 = findViewById(R.id.addClass3_2);
+        spinnerList.add(class3_2);
+        class3_3 = findViewById(R.id.addClass3_3);
+        spinnerList.add(class3_3);
+        class3_4 = findViewById(R.id.addClass3_4);
+        spinnerList.add(class3_4);
+        class3_5 = findViewById(R.id.addClass3_5);
+        spinnerList.add(class3_5);
+        class4_1 = findViewById(R.id.addClass4_1);
+        spinnerList.add(class4_1);
+        class4_2 = findViewById(R.id.addClass4_2);
+        spinnerList.add(class4_2);
+        class4_3 = findViewById(R.id.addClass4_3);
+        spinnerList.add(class4_3);
+        class4_4 = findViewById(R.id.addClass4_4);
+        spinnerList.add(class4_4);
+        class4_5 = findViewById(R.id.addClass4_5);
+        spinnerList.add(class4_5);
+        class5_1 = findViewById(R.id.addClass5_1);
+        spinnerList.add(class5_1);
+        class5_2 = findViewById(R.id.addClass5_2);
+        spinnerList.add(class5_2);
+        class5_3 = findViewById(R.id.addClass5_3);
+        spinnerList.add(class5_3);
+        class5_4 = findViewById(R.id.addClass5_4);
+        spinnerList.add(class5_4);
+        class5_5 = findViewById(R.id.addClass5_5);
+        spinnerList.add(class5_5);
+        class6_1 = findViewById(R.id.addClass6_1);
+        spinnerList.add(class6_1);
+        class6_2 = findViewById(R.id.addClass6_2);
+        spinnerList.add(class6_2);
+        class6_3 = findViewById(R.id.addClass6_3);
+        spinnerList.add(class6_3);
+        class6_4 = findViewById(R.id.addClass6_4);
+        spinnerList.add(class6_4);
+        class6_5 = findViewById(R.id.addClass6_5);
+        spinnerList.add(class6_5);
+        class7_1 = findViewById(R.id.addClass7_1);
+        spinnerList.add(class7_1);
+        class7_2 = findViewById(R.id.addClass7_2);
+        spinnerList.add(class7_2);
+        class7_3 = findViewById(R.id.addClass7_3);
+        spinnerList.add(class7_3);
+        class7_4 = findViewById(R.id.addClass7_4);
+        spinnerList.add(class7_4);
+        class7_5 = findViewById(R.id.addClass7_5);
+        spinnerList.add(class7_5);
+        class8_1 = findViewById(R.id.addClass8_1);
+        spinnerList.add(class8_1);
+        class8_2 = findViewById(R.id.addClass8_2);
+        spinnerList.add(class8_2);
+        class8_3 = findViewById(R.id.addClass8_3);
+        spinnerList.add(class8_3);
+        class8_4 = findViewById(R.id.addClass8_4);
+        spinnerList.add(class8_4);
+        class8_5 = findViewById(R.id.addClass8_5);
+        spinnerList.add(class8_5);
+    }
+
+    private void setAdapters(ArrayList<Spinner> spinnerList, ArrayAdapter arrayAdapter) {
+        for (Spinner s : spinnerList) {
+            s.setAdapter(arrayAdapter);
+        }
     }
 }
